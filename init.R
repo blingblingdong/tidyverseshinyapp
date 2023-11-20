0ff02d156fef6c14a2998a1eec0b1fb3d977d629
blingblingdong <whoareyouman0.4@gmail.com>
2023-11-20 18:48:15 +0800

    ddd

--- init.R
+++ init.R
@@ -3,8 +3,8 @@
 # Example R code to install packages if not already installed
 #
 
-my_packages = c('shiny', 'shinyjs', 'fireData', 'promises', 'future', 'DT', 'markdown','data.table','tidyverse',
-                'showtext','bs4Dash','waiter')
+my_packages = c('shiny', 'shinyjs', 'promises', 'future', 'DT', 'markdown','data.table','dplyr','tidyr','readr','tibble',
+                'showtext','bs4Dash','waiter','purrr')
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
